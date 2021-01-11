0ad082c0d4aa6b4e2681067024d6974ee8a9c942
useonglee <useong0830@gmail.com>
2021-01-11 23:19:36 +0900

    1월 11일 update

--- _site/assets/js/lunr/lunr-store.js
+++ _site/assets/js/lunr/lunr-store.js
@@ -1,13 +1,19 @@
 var store = [{
-        "title": "1월 10일 TIL",
-        "excerpt":"##드디어 깃블로그를 마치다. 금요일 오후 1시부터 시작을해서 드디어 깃블로그를 어느정도 알게 된 것 같다. 아직도 많이 미숙하지만 3일동안 삽질한 덕에 대충은 어떻게 돌아가는지 알게 되었다. 역시 삽질해봐야 알게 되는 것인가….. 테마를 다운받고 레퍼지토리에 커밋하는건 어렵지않았다. 하지만!!!!!! _conpig 파일을 수정하고 bundle exec jekyll serve 로 블로그를 수정하려고 하면 계속 오류가 났다....","categories": ["TIL"],
-        "tags": ["TIL"],
-        "url": "http://localhost:4000/til/TIL/",
-        "teaser": null
-      },{
         "title": "나의 첫 포스트",
         "excerpt":"나의 첫 포스트라고 했지만 깃블로그를 위해 삽질을 3일 동안 했다.   지우고 다시 깔고를 수십번 반복했다. 이 포스트 또한 또 지워질 수 도 있다.   미칠 것 같다. 깃블로그 너무 어렵다.   ","categories": ["Blog"],
         "tags": ["Posts"],
         "url": "http://localhost:4000/blog/first-post/",
+        "teaser": null
+      },{
+        "title": "1월 10일 TIL #1",
+        "excerpt":"##드디어 깃블로그를 마치다. 금요일 오후 1시부터 시작을해서 드디어 깃블로그를 어느정도 알게 된 것 같다. 아직도 많이 미숙하지만 3일동안 삽질한 덕에 대충은 어떻게 돌아가는지 알게 되었다. 역시 삽질해봐야 알게 되는 것인가….. 테마를 다운받고 레퍼지토리에 커밋하는건 어렵지않았다. 하지만!!!!!! _conpig 파일을 수정하고 bundle exec jekyll serve 로 블로그를 수정하려고 하면 계속 오류가 났다....","categories": ["Pre-Course"],
+        "tags": ["TIL","CodeStates"],
+        "url": "http://localhost:4000/pre-course/TIL/",
+        "teaser": null
+      },{
+        "title": "1월 11일 TIL #2",
+        "excerpt":"코드 변환(고차함수) + 반복문 18번 1.고차함수 문제 : 아래 코드를 reduce메서드 한 번만 사용한 코드로 변경하여 스스로 작성하고, 직접 설명한 내용을 적어주세요. const filterMaped = arr.filter(x =&gt; x % 2 !== 0).map(x =&gt; x * 2); console.log(filterMaped) // [2, 6, 10] reduce를 사용한 코드로 변경하기에 앞서 나는 먼저 어떤 배열이었길래...","categories": ["Pre-Course"],
+        "tags": ["TIL","CodeStates"],
+        "url": "http://localhost:4000/pre-course/TIL/",
         "teaser": null
       }]
